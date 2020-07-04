05dd900d909a2c04b71b0c98ff9edf133b88f2c6
roxket <ox_h4d2@hotmail.es>
2020-07-04 23:05:44 +0200

    Setting saving User endopoint

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,42 @@
 const bcrypt = require("bcrypt-node");
-const user = require("../models/user");
+const User = require("../models/user");
+const { restart } = require("nodemon");
 
 function signUp(req, res) {
-  console.log("Endpoint de signUp");
+  const user = new User();
+  const { name, lastName, email, password, repeatPassword } = req.body;
+  user.name = name;
+  user.lastName = lastName;
+  user.email = email;
+  user.role = "admin";
+  user.active = false;
+
+  if (!password || !repeatPassword) {
+    res.status(404).send({ message: "Introduce la contraseña." });
+  } else {
+    if (password !== repeatPassword) {
+      res.status(404).send({ message: "Las contraseñas no coinciden." });
+    } else {
+      bcrypt.hash(password, null, null, function (err, hash) {
+        if (err) {
+          res.status(500).send({ message: "Error encriptación contraseña." });
+        } else {
+          user.password = hash;
+          user.save((err, userStored) => {
+            if (err) {
+              res.status(500).send({ message: "El usuario ya existe: " + err });
+            } else {
+              if (!userStored) {
+                res.status(404).send({ message: "Error al crear el usuario." });
+              } else {
+                res.status(200).send({ user: userStored });
+              }
+            }
+          });
+        }
+      });
+    }
+  }
 }
 
 module.exports = {
